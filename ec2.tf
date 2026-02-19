099368f09a39a2c28cdfefcfa05c3193ece424cf
AayushKarwa <ayushkarwa18@gmail.com>
2026-02-19 17:14:15 +0530

    added ansible storybook

--- ec2.tf
+++ ec2.tf
@@ -1,76 +1,119 @@
-# Key pair login
+############################################
+# LOCALS
+############################################
+
+locals {
+  instance_config = {
+    ansible-master = {
+      ami           = "ami-019715e0d74f695be" # Ubuntu
+      instance_type = "t2.medium"
+      ssh_user      = "ubuntu"
+    }
+
+    ansible-node-1 = {
+      ami           = "ami-0ffef61f6dc37ae89" # RedHat
+      instance_type = "t2.micro"
+      ssh_user      = "ec2-user"
+    }
+
+    ansible-node-2 = {
+      ami           = "ami-0a15c80c30715cc92" # Debian
+      instance_type = "t2.micro"
+      ssh_user      = "admin"
+    }
+
+    ansible-node-3 = {
+      ami           = "ami-019715e0d74f695be" # Ubuntu
+      instance_type = "t2.micro"
+      ssh_user      = "ubuntu"
+    }
+  }
+}
+
+############################################
+# KEY PAIR
+############################################
+
 resource "aws_key_pair" "terra-key" {
-  key_name   = "terra-key-ec2"
-  public_key = file("terra-key.pub")
-}
-# VPC and security group
-resource "aws_default_vpc" "default"{
-    tags = {
-      Name: "default_vpc"
-    }
+  key_name   = "terra-key-ansible"
+  public_key = file("terra-key-ansible.pub")
 }
 
-resource "aws_security_group" "automated-sg"{
-    name = "automated-sg"
-    description = "sg using terraform"
-    vpc_id = aws_default_vpc.default.id
-    # inbound rules
-    ingress{
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        description = "ssh port open"
-    }
+############################################
+# DEFAULT VPC
+############################################
 
-    ingress{
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        description = "http port open"
-    }
-    ingress{
-        from_port = 8000
-        to_port = 8000
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        description = "app port open"
-    }
-
-    # outbound rules
-    egress{
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        description = "outbound rules open for all"
-    }
-} 
-
-# ec2 instance 
-
-resource "aws_instance" "terraform-ec2"{
-    key_name = aws_key_pair.terra-key.key_name
-    for_each = tomap({
-        ansible-master = "ami-019715e0d74f695be" #ubuntu
-        ansible-node-1 = "ami-0ffef61f6dc37ae89" #redhat
-        ansible-node-2 = "ami-0a15c80c30715cc92" #debian
-        ansible-node-3 = "ami-019715e0d74f695be" #ubuntu
-    })
-    security_groups = [ aws_security_group.automated-sg.name ]
-    instance_type = "t2.micro"
-    ami = each.value
-    root_block_device {
-      volume_size = 10
-      volume_type = "gp3"
-    }
-    depends_on = [ aws_default_vpc.default, aws_key_pair.terra-key, aws_security_group.automated-sg ]
-    user_data = file("init.sh")
-    tags = {
-        Name = each.key
-    }
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "default_vpc"
+  }
 }
 
+############################################
+# SECURITY GROUP
+############################################
 
+resource "aws_security_group" "automated-sg" {
+  name        = "automated-sg"
+  description = "sg using terraform"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "ssh port open"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http port open"
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "app port open"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow all outbound"
+  }
+}
+
+############################################
+# EC2 INSTANCES
+############################################
+
+resource "aws_instance" "terraform-ec2" {
+  for_each = local.instance_config
+
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
+  key_name      = aws_key_pair.terra-key.key_name
+
+  vpc_security_group_ids = [aws_security_group.automated-sg.id]
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+  instance_initiated_shutdown_behavior = "stopped"
+
+  user_data = file("init.sh")
+
+  tags = {
+    Name = each.key
+  }
+}
 
--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,12 @@
-output "ec2_public_ip" {
-  value = {
-    for key, instance in aws_instance.terraform-ec2: 
-    key => instance.public_ip
-  }
-}
-
-output "ec2_public_dns" {
-  value = {
-    for key, instance in aws_instance.terraform-ec2: 
-    key => instance.public_dns
-  }
-}
-
-output "ec2_private_ip" {
+output "ec2_full_details" {
   value = {
     for key, instance in aws_instance.terraform-ec2 :
-    key => instance.private_ip
+    key => {
+      public_ip  = instance.public_ip
+      private_ip = instance.private_ip
+      ami_id     = instance.ami
+      instance_type = instance.instance_type
+      ssh_user   = local.instance_config[key].ssh_user
+    }
   }
 }
